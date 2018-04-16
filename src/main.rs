15d1537d5c7a5d910cff847ed8ff70088ba29c7e
saresend <saresend@usc.edu>
2018-04-16 12:43:22 -0700

    matching against basic commands

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+
+extern crate clap;
+
+use clap::{Arg, App};
+
+fn main() {
+
+    let matches = App::new("Brainlag")
+        .version("1.0")
+        .author("Samuel Resendez saresend@usc.edu")
+        .about("Brainlag is a dead easy CLI for saving values so you don't have to remember them")
+        .arg(Arg::with_name("CMD").index(1).required(true))
+        .arg(Arg::with_name("PARAM").index(2))
+        .get_matches();
+
+    match matches.value_of("CMD") {
+        Some("ls") => {
+            println!("Printing List of Stored Values");
+        }
+        Some("get") => {
+            println!("Getting stored value");
+        }
+
+        Some("set") => {
+            println!("Setting value");
+        }
+        _ => {
+            println!("Unrecognized command, please use --help for usage.");
+            return;
+        }
+    }
+
+
+
+
+}
